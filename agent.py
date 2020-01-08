88fdaaa780b0501dc373908c2bcabbc889bb3ffe
Victor <nikki.solution@gmail.com>
2020-01-08 20:05:58 +0500

    Сократил код

--- agent.py
+++ agent.py
@@ -5,16 +5,9 @@ import time
 
 starttime=time.time()
 logging.basicConfig(filename="logs/main.log", level=logging.INFO)
-dataFileHost = 'https://dev.solvers.group/ocenkulator/hosts'
+dataFileHost = 'https://dev.solvers.group/defender/hosts'
 dataFileNameHost = 'hosts'
 
-def passage(file_name, folder):
-    for element in os.scandir(folder):
-        if element.is_file():
-            if element.name == file_name:
-                yield folder
-        else:
-            yield from passage(file_name, element.path)
 
 def loadFile():
     if os.access(dataFileNameHost, os.F_OK) == True:
@@ -31,5 +24,4 @@ while True:
         filename = wget.download(dataFileHost)
         os.rename(filename, u'' + os.getcwd() + '/' + filename)
 
-    print('hosts', os.getcwd())
     time.sleep(600.0 - ((time.time() - starttime) % 600.0))
